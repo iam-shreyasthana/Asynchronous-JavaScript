1dc448295b001b66e955aab33a4d83039d4fa1a1
iam-shreyasthana <shreyasthana545@gmail.com>
2022-12-21 22:33:00 +0530

    added Promise.all functionality

--- promises.js
+++ promises.js
@@ -33,3 +33,15 @@ const createPost = (post) => {
 
 
 createPost({ title: 'Post Three', body : 'This is post three'}).then(getPosts).catch(err => console.log(err));
+
+// Promise.all
+
+const promise1 = Promise.resolve('Hello World');
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 2000, 'Goodbye')
+});
+
+Promise.all([promise1, promise2, promise3]).then(values => {
+    console.log(values);
+});
